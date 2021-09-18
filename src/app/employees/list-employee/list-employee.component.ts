70cfa53a1bb8880468de862a940c7567185334c8
Zouhair <ettarak.zouhair@gmail.com>
2021-09-18 22:02:49 +0100

    List employee simple method

--- src/app/employees/list-employee/list-employee.component.ts
+++ src/app/employees/list-employee/list-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from './../../models/employee';
 
 @Component({
   selector: 'app-list-employee',
@@ -6,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-employee.component.css']
 })
 export class ListEmployeeComponent implements OnInit {
+
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      phoneNumber: 2345978640,
+      email: 'mark@pragimtech.com',
+      dateOfBirth: new Date('10/25/1988'),
+      department: 'IT',
+      isActive: true,
+      photoPath: 'assets/images/mark.png'
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      phoneNumber: 2345978640,
+      email: 'mary.1547@gmail.com',
+      dateOfBirth: new Date('11/20/1979'),
+      department: 'HR',
+      isActive: true,
+      photoPath: 'assets/images/mary.png'
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      phoneNumber: 5432978640,
+      email: 'johne@hoymail.fr',
+      dateOfBirth: new Date('3/25/1976'),
+      department: 'IT',
+      isActive: false,
+      photoPath: 'assets/images/john.png'
+    },
+  ];
 
   constructor() { }
 
