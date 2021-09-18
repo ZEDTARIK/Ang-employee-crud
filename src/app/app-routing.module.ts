f796941c735ebb39cb0edb6dc1648e43ca7b0c59
Zouhair <ettarak.zouhair@gmail.com>
2021-09-18 22:30:26 +0100

    Routing router-outlet , create navbar inverse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import { ListEmployeeComponent } from './employees/list-employee/list-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ListEmployeeComponent },
+  { path: "list-employee", component: ListEmployeeComponent },
+  { path: "create-employee", component: CreateEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListEmployeeComponent } from './employees/list-employee/list-employee.component';
 import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeeComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
